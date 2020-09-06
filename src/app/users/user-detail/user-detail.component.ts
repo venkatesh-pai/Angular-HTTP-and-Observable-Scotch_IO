a03428f2063791a8629df8f0fc31c847c88ea42d
venkatesh-pai <ksvenkatesh1990@gmail.com>
2020-09-06 18:45:15 +0530

    10 - User Detail Component

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/shared/models/user.model';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  user: User;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService) {
+
+    const id = +this.route.snapshot.paramMap.get('id');
+
+    this.userService.getUser(id)
+      .subscribe(
+        user => this.user = user,
+        error => this.errorMessage = error
+      );
+  }
 
   ngOnInit(): void {
+  }
+
+  navigateToUserList(): void {
+    this.router.navigate(['users']);
   }
 
 }
